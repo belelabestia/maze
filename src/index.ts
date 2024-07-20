b0d58aea07ec22f1f7c0726499f649855c48885a
Marco Bellezza <jasone.mb@outlook.com>
2024-07-20 16:23:16 +0200

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('Proof');
+import { Maze } from './maze';
+import { Size } from './size';
+
+const main = () => {
+  const size = Size(process.argv[0]);
+  if (size === null) return;
+
+  const maze = Maze(size);
+  console.log(maze);
+};
+
+main();
